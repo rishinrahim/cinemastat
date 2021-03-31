a180b568c255acf201f3f552f8a830675d4e1a3a
Rishin <rishin07@gmail.com>
2021-03-31 18:04:25 +0530

    Update status.js

--- charts/status.js
+++ charts/status.js
@@ -1,1 +1,14 @@
-var ctx = document.getElementById('chart1')
+$( document ).ready(function() {
+    console.log($("#st-total").val());
+    $("#st-total").text("6400");
+    $("#st-released").text("5213");
+    $("#st-db").text("311");
+    $("#st-unreleased").text("490");
+    $("#st-shoot").text("386");
+
+
+
+
+});
+
+
